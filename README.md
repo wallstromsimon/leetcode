54d230ad29eb1b9c9fe44168735e8ad74df74faf
Simon Wallstrom <simon.wallstrom@telavox.se>
2020-04-21 17:52:24 +0200

    twoSum done
    Som updates to readme about testing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # leetcode
-Collection of leetcode problems (and solutions).
+Collection of [leetcode](https://leetcode.com/) problems (and solutions).
+
+## Run test in go
+To run x_test.go run
+```shell script
+go test -v x_test.go
+```
+to run all functions (tests) on the form `func TestXxx(*testing.T)`
+
+Read more about go testing https://golang.org/pkg/testing/
---
+++ twoSum_test.go
@@ -0,0 +1,55 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	expectedResult := []int{0, 1}
+
+	if !equal(result, expectedResult) {
+		t.Errorf("Got %v; expected %v", result, expectedResult)
+	}
+}
+
+func TestTwoSum2(t *testing.T) {
+	nums := []int{3, 2, 4}
+	target := 6
+	result := twoSum(nums, target)
+	expectedResult := []int{1, 2}
+
+	if !equal(result, expectedResult) {
+		t.Errorf("Got %v; expected %v", result, expectedResult)
+	}
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if i == j {
+				continue
+			}
+			if nums[i] + nums[j] == target {
+				return []int{i,j}
+			}
+		}
+	}
+	return nil
+}
+
+// Equal tells whether a and b contain the same elements.
+// A nil argument is equivalent to an empty slice.
+func equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
